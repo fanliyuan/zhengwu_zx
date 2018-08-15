b63aefc183be1c6e12d99cc8a67b851785975955
ChouEric <choueric@foxmail.com>
2018-08-15 09:40:34 +0800

    联调11, 从服务器获取路由和菜单的权限

--- src/utils/authority.js
+++ src/utils/authority.js
@@ -1,3 +1,6 @@
+import apis from '../api'
+
+const { getPermission } = apis 
 // use localStorage to store the authority info, which might be sent from server in actual project.
 export function getAuthority() {
   return localStorage.getItem('antd-pro-authority') || 'guest'
@@ -5,4 +8,22 @@ export function getAuthority() {
 
 export function setAuthority(authority = 'guest') {
   return localStorage.setItem('antd-pro-authority', authority)
+}
+
+// 从服务器获取权限
+export async function getPermissions() {
+  let permissions = []
+  try {
+    const response = await getPermission()
+    if (+response.code === 0) {
+      permissions = response.result.datas
+    } else {
+      throw Error('权限返回错误')
+    }
+  } catch (error) {
+   // eslint-disable-next-line 
+   console.log(error)
+   permissions = []
+  }
+  return permissions
 }
