c9d6ca3f5a81be134fe30756374108612825485c
李学川 <lixuechuan@youedata.com>
2018-09-10 16:24:28 +0800

    Update devops.sh

--- devops.sh
+++ devops.sh
@@ -3,4 +3,4 @@
   fi
   npm run build
   
-  echo "RUN sed -i 's#location / {#location / {\n\t\ttry_files $uri /index.html;#' /etc/nginx/conf.d/default.conf" >> Dockerfile
+  echo "RUN sed -i 's#location / {#location / {\n\t\ttry_files \$uri /index.html;#' /etc/nginx/conf.d/default.conf" >> Dockerfile
