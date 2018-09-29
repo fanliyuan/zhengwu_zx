d2f87080fdcfc14f911fbd7d82c3de599cb37477
ChouEric <choueric@foxmail.com>
2018-09-29 16:31:46 +0800

    二期迭代v9.2.1, 目录树采用c3calc计算,并且和less兼容

--- src/api/exchangeManagement/index.js
+++ src/api/exchangeManagement/index.js
@@ -14,8 +14,8 @@ const module = {
 }
 module.apis.forEach(item => {
   if (!item.baseHost) {
-    // item.baseHost = 'http://192.168.100.16:8803'
-    item.baseHost = 'http://testresource.tpaas.youedata.com'
+    item.baseHost = 'http://192.168.100.16:8803'
+    // item.baseHost = 'http://testresource.tpaas.youedata.com'
   }
 })
 export default module
