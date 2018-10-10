e6455b5eeb2d47089362b1a3b8e7873d997280e2
ChouEric <choueric@foxmail.com>
2018-10-10 18:03:00 +0800

    迭代3.3.1,优化显示问题

--- src/components/ECharts/Tree/index.js
+++ src/components/ECharts/Tree/index.js
@@ -2,11 +2,8 @@ import React, { Component } from 'react'
 
 // 引入 ECharts 主模块
 import echarts from 'echarts/lib/echarts'
-// 引入图
 import 'echarts/lib/chart/graph'
-// 引入提示框和标题和图例组件
 import 'echarts/lib/component/tooltip'
-// import 'echarts/lib/component/title'
 import 'echarts/lib/component/legend'
 
 export default class Tree extends Component{
