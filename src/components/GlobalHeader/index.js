8f69c361cdcef8d84142ed073ab276ab9941c5be
fanliyuan <fanliyuan2011@163.com>
2018-10-17 09:51:21 +0800

    修改头部消息

--- src/components/GlobalHeader/index.js
+++ src/components/GlobalHeader/index.js
@@ -4,10 +4,14 @@ import moment from 'moment'
 import groupBy from 'lodash/groupBy'
 import Debounce from 'lodash-decorators/debounce'
 import { Link } from 'dva/router'
+// import { connect } from 'dva'
 import NoticeIcon from '../NoticeIcon'
 import HeaderSearch from '../HeaderSearch'
 import styles from './index.less'
 
+// @connect(({}) => ({
+
+// }))
 export default class GlobalHeader extends PureComponent {
   componentWillUnmount() {
     this.triggerResizeEvent.cancel()
@@ -138,18 +142,18 @@ export default class GlobalHeader extends PureComponent {
               emptyText="你已查看所有通知"
               emptyImage="https://gw.alipayobjects.com/zos/rmsportal/wAhyIChODzsoKIOBHcBk.svg"
             />
-            <NoticeIcon.Tab
+            {/* <NoticeIcon.Tab
               list={noticeData['消息']}
               title="消息"
               emptyText="您已读完所有消息"
               emptyImage="https://gw.alipayobjects.com/zos/rmsportal/sAuJeJzSKbUmHfBQRzmZ.svg"
-            />
-            <NoticeIcon.Tab
+            /> */}
+            {/* <NoticeIcon.Tab
               list={noticeData['待办']}
               title="待办"
               emptyText="你已完成所有待办"
               emptyImage="https://gw.alipayobjects.com/zos/rmsportal/HsIsxMZiWKrNUavQUXqx.svg"
-            />
+            /> */}
           </NoticeIcon>
           {currentUser.name ? (
             <Dropdown overlay={menu}>
