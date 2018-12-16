c84a6a3878047ed4a8febab90ae45d4c37beaa9f
ChouEric <choueric@foxmail.com>
2018-12-16 17:48:11 +0800

    切换为线上上传地址

--- src/api/config.js
+++ src/api/config.js
@@ -2,14 +2,14 @@
  * @Author: ChouEric
  * @Date: 2018-08-05 17:18:26
  * @Last Modified by: ChouEric
- * @Last Modified time: 2018-12-16 16:39:27
+ * @Last Modified time: 2018-12-16 17:39:03
  * @Description: 全局接口配置
  */
 const baseConfig = {
   // uploadServer: 'http://192.168.100.16:8804',
   // downloadServer: 'http://192.168.100.16:8804',
-  uploadServer: 'http://192.168.100.16:2181/zwjh/api/v1', // 文件上传的地址
-  // uploadServer:'/portalapi',
+  // uploadServer: 'http://192.168.100.16:2181/zwjh/api/v1', // 文件上传的地址
+  uploadServer:'/portalapi/zwjh/api/v1',
   downloadServer: '/portalapi', // 文件下载的地址
   portalsServer: 'www.baidu.com/portals', // 门户网站的文章地址
   // apiHost: 'http://api.tpaas.youedata.com',
